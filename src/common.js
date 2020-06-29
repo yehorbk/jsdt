06157990c59a550da17bfe20f3ea28453011ab94
Артем Бокий <bokiiartem@gmail.com>
2020-06-29 12:18:52 +0300

    Added readFile and writeFile functions

--- src/common.js
+++ src/common.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const fs = require('fs');
+
+const readFile = async filePath => {
+    return new Promise(resolve => {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) return err;
+            return resolve(data);
+        });
+    });
+};
+
+const writeFile = (filePath, string) => {
+    fs.writeFile(filePath, string, (err) => {
+        if (err) return err;
+    });
+};
+
+module.exports = { readFile, writeFile };
